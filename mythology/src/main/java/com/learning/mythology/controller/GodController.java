97be301e292950eea59114661cb6ecebc3b35030
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2019-02-20 21:59:54 +0100

    Added exception handling to GodController

--- mythology/src/main/java/com/learning/mythology/controller/GodController.java
+++ mythology/src/main/java/com/learning/mythology/controller/GodController.java
@@ -2,7 +2,11 @@ package com.learning.mythology.controller;
 
 import com.learning.mythology.model.God;
 import com.learning.mythology.service.GodService;
+import com.learning.mythology.util.ServiceError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,5 +43,11 @@ public class GodController {
     public @ResponseBody Object deleteGod(@PathVariable(value = "id") Integer id){
         godService.deleteGod(id);
         return null;
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ServiceError> handle(RuntimeException e){
+        ServiceError serviceError = new ServiceError(HttpStatus.OK.value(), e.getMessage());
+        return new ResponseEntity<>(serviceError,HttpStatus.OK);
     }
 }
